b703f9de324163ca48a97d96ee6b18dd864a6d47
PranshuCs <pranshu22117@iiitd.ac.in>
2024-06-22 20:30:55 +0530

    first one, khadkau kyaaa

---
+++ src/constants/constant.js
@@ -0,0 +1,5 @@
+
+export const logo = 'https://previews.123rf.com/images/anwarsikumbang/anwarsikumbang1912/anwarsikumbang191200453/136111379-kitchen-utensil-master-chef-character-cartoon-art-logo-icon-vector.jpg';
+
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
